2d5bb9e2dc5bf4319f784d8e7895ef5ddfc2d11e
khobster <152924497+khobster@users.noreply.github.com>
2024-06-22 03:25:41 -0400

    Update plunko.js

--- plunko.js
+++ plunko.js
@@ -58,7 +58,7 @@ function updateStreakAndGenerateSnippetStandard(isCorrect, playerName, resultEle
             resultElement.innerHTML = "<span class='kaboom'>PLUNKO!</span>";
             const encodedPlayers = encodeURIComponent(lastThreeCorrectStandard.join(','));
             const shareLink = `https://khobster.github.io/plunko?players=${encodedPlayers}`;
-            let shareText = `3 in a row! That's a PLUNK🏀!\n\nChallenge your pals with the same PLUNK🏀:\n${shareLink}`;
+            let shareText = `3 in a row! That's a PLUNK🏀!\n\nChallenge your pals with this PLUNK🏀:\n${shareLink}`;
             document.getElementById('shareSnippet').innerHTML = shareText;
             document.getElementById('copyButton').style.display = 'inline-block';
             document.getElementById('returnButton').style.display = 'inline-block';
@@ -106,7 +106,7 @@ function updateStreakAndGenerateSnippetURL(isCorrect, playerName, resultElement,
             // Add share snippet and buttons
             const encodedPlayers = encodeURIComponent(lastThreeCorrectURL.join(','));
             const shareLink = `https://khobster.github.io/plunko?players=${encodedPlayers}`;
-            let shareText = `3 in a row! That's a PLUNK🏀!\n\nChallenge your pals with the same PLUNK🏀:\n${shareLink}`;
+            let shareText = `3 in a row! That's a PLUNK🏀!\n\nChallenge your pals with this PLUNK🏀:\n${shareLink}`;
             setTimeout(() => {
                 document.getElementById('shareSnippet').innerHTML = shareText;
                 document.getElementById('copyButton').style.display = 'inline-block';
